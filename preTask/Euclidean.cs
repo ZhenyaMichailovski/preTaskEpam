74566168205ae58cf7464efaa21c1ac650c2883f
ZhenyaMichailovski <72973567+ZhenyaMichailovski@users.noreply.github.com>
2021-09-30 14:27:17 +0300

    add euclid's algorithm for two, three, four and five numbers

---
+++ preTask/Euclidean.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace preTask
+{
+    class Euclidean
+    {
+        public int FindGDC(int first, int second)
+        {
+            while (first != second)
+                if (first != second)
+                    first -= second;
+                else
+                    second -= first;
+
+            return first;
+        }
+
+        public int FindGDC(int first, int second, int third)
+        {
+            var result = FindGDC(first, second);
+            return FindGDC(result, third);
+        }
+
+        public int FindGDC(int first, int second, int third, int fourth)
+        {
+            var result = FindGDC(first, second, third);
+            return FindGDC(result, fourth);
+        }
+
+        public int FindGDC(int first, int second, int third, int fourth, int fifth)
+        {
+            var result = FindGDC(first, second, third, fourth);
+            return FindGDC(result, fifth);
+        }
+    }
+}
